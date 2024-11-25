82d7471245cd011ad1f2021e0e2dcb49c68e740b
realdegrees <18383355+realdegrees@users.noreply.github.com>
2024-11-25 17:41:17 +0100

    feature: switch to unity event system for input system

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -15,33 +15,45 @@ public class InputManager : Manager<InputManager>
     public bool AttackCharged { get; private set; } // Same for attacks, charge attacks are a common mechanic
     public bool AttackReleased { get; private set; }
 
-    private InputAction moveAction;
-    private InputAction crouchAction;
-    private InputAction jumpAction;
-    private InputAction attackAction;
-
-    protected override void Awake()
+    // Gets called after Awake in the Manager class
+    protected override void Init()
     {
-        base.Awake();
-
         PlayerInput = GetComponent<PlayerInput>();
+    }
 
-        moveAction = PlayerInput.actions["Move"];
-        crouchAction = PlayerInput.actions["Crouch"];
-        jumpAction = PlayerInput.actions["Jump"];
-        attackAction = PlayerInput.actions["Attack"];
+    public void Move(InputAction.CallbackContext context)
+    {
+        Movement = context.action.ReadValue<Vector2>();
+    }
+    public void Jump(InputAction.CallbackContext context)
+    {
+        JumpPressed = context.action.WasPressedThisFrame();
+        JumpCharged = context.action.ReadValue<float>() > 0;
+        JumpReleased = context.action.WasReleasedThisFrame();
+    }
+
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        CrouchHeld = context.action.ReadValue<float>() > 0;
+    }
+    public void Attack(InputAction.CallbackContext context)
+    {
+        AttackPressed = context.action.WasPressedThisFrame();
+        AttackCharged = context.action.ReadValue<float>() > 0;
+        AttackReleased = context.action.WasReleasedThisFrame();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Movement = moveAction.ReadValue<Vector2>();
-        JumpPressed = jumpAction.WasPressedThisFrame();
-        JumpCharged = jumpAction.ReadValue<float>() > 0;
-        JumpReleased = jumpAction.WasReleasedThisFrame();
-        CrouchHeld = crouchAction.ReadValue<float>() > 0;
-        AttackPressed = attackAction.WasPressedThisFrame();
-        AttackCharged = attackAction.ReadValue<float>() > 0;
-        AttackReleased = attackAction.WasReleasedThisFrame();
+        return;
+        // Movement = moveAction.ReadValue<Vector2>();
+        // JumpPressed = jumpAction.WasPressedThisFrame();
+        // JumpCharged = jumpAction.ReadValue<float>() > 0;
+        // JumpReleased = jumpAction.WasReleasedThisFrame();
+        // CrouchHeld = crouchAction.ReadValue<float>() > 0;
+        // AttackPressed = attackAction.WasPressedThisFrame();
+        // AttackCharged = attackAction.ReadValue<float>() > 0;
+        // AttackReleased = attackAction.WasReleasedThisFrame();
     }
 }
