cbec4cc3f2f417ea5324e821bfaae98d66d9619d
realdegrees <18383355+realdegrees@users.noreply.github.com>
2025-03-30 18:26:51 +0200

    get scenario from server, prepend scenario to id of log sent to server, load different levels based on scenario

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Manager;
 using UnityEngine;
 
@@ -5,19 +6,56 @@ using UnityEngine;
 
 public class GameManager : Manager<GameManager>
 {
-    protected override void Init()
-    {
-        Debug.Log("GameManager Awake");
-    }
+    private string scenario = null;
+    [HideInInspector] public string id;
+    [HideInInspector] public string Scenario => scenario;
+
+    [Header("Debug")]
+    public bool UseRandomUserId = false; // Set this to true to use a random user ID
+
     // Start is called before the first frame update
-    void Start()
+    protected override void Awake()
     {
-
+        id = UseRandomUserId ? System.Guid.NewGuid().ToString("N")[..8] : SystemInfo.deviceUniqueIdentifier[..8];
+        StartCoroutine(FetchScenario());
+        base.Awake();
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FetchScenario()
     {
+        using var webClient = new System.Net.WebClient();
+        webClient.Headers[System.Net.HttpRequestHeader.ContentType] = "application/json";
+        webClient.Headers[System.Net.HttpRequestHeader.Authorization] = "Bearer ur-game-engineering-2025";
+        webClient.Encoding = System.Text.Encoding.UTF8;
+
+        var downloadTask = System.Threading.Tasks.Task.Run(() =>
+            webClient.DownloadString("https://ge-log-api.realdegrees.dev/scenario/" + id)
+        );
+
+        downloadTask.GetAwaiter().OnCompleted(() =>
+        {
+            if (downloadTask.IsCompletedSuccessfully)
+            {
+                scenario = downloadTask.Result;
+            }
+            else
+            {
+                scenario = Random.value > 0.5f ? "A" : "B"; // Default scenario if the request fails or times out
+            }
+        });
+
+        yield return new WaitForSeconds(3f);
+        if (downloadTask.IsCompleted)
+        {
+            yield return null;
+        }
+        else
+        {
+            downloadTask.Dispose();
+            scenario = Random.value > 0.5f ? "A" : "B"; // Default scenario if the request fails or times out
+
+            yield return null;
+        }
 
     }
 }
